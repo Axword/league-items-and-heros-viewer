55bdecc481cf012468651df3db60da2606dd0757
Axword <xAxwordx@gmail.com>
2024-10-10 11:55:04 +0200

    feat: add all the stuff from UI app and more

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, jsonify, send_from_directory
 import os
 import json
 import requests
 from PIL import Image
 import io
+import re
+
 
 class LeagueViewer:
     def __init__(self):
@@ -13,34 +15,84 @@ class LeagueViewer:
         self.version_dir = os.path.join(self.cache_dir)
         self.language = "pl_PL"
         self.latest_version = self.get_latest_version()
-
-        # Create cache directories if they do not exist
+        self.sorted_unique_items = {"en_US": None, 'pl_PL': None}
+        self.heores = {"en_US": None, 'pl_PL': None}
         os.makedirs(self.image_cache_dir, exist_ok=True)
 
-        # Setup routes
         self.setup_routes()
+        self.translations = {
+            'en_US': {
+                'back_to_champions': 'Back to champions',
+                'back_to_items': 'Back to items',
+                'welcome': 'Welcome to League Viewer',
+                'champions': 'Champions',
+                'items': 'Items',
+                'language': "Language",
+                "polish": "Polish",
+                "english": "English",
+                'stats': "Stats",
+                'tags': "Tags",
+                'skins': "Skins",
+                'skills': "Skills",
+                'description': "Description",
+                'builds_into': "Builds Into",
+                'cost': "Cost",
+            },
+            'pl_PL': {
+                'back_to_champions': 'Powrót do bohaterów',
+                'back_to_items': 'Powrót do przedmiotów',
+                'welcome': 'Witamy w League Viewer',
+                'champions': 'Bohaterowie',
+                'items': 'Przedmioty',
+                'language': "Język",
+                "polish": "Polski",
+                "english": "Angielski",
+                'stats': "Statystyki",
+                'tags': "Tagi",
+                'skins': "Skiny",
+                'skills': "Umiejętności",
+                'description': "Opis",
+                "builds_into": "Buduje się w",
+                "cost": 'Koszt'
+            }
+        }
 
     def setup_routes(self):
         @self.app.route('/')
         def index():
-            return render_template('index.html')
-    
+            return render_template('index.html', language=self.language, translations=self.translations[self.language])
+
+        @self.app.route('/set_language', methods=['POST'])
+        def set_language():
+            self.language = request.form.get('language')
+            return redirect(request.referrer)
+
         @self.app.route('/champions')
         def champions():
             champions_data = self.get_data("championFull")
-            for champion_id, champion in champions_data.items():
-                image_url = f"https://ddragon.leagueoflegends.com/cdn/{self.latest_version}/img/champion/{champion_id}.png"
-                self.fetch_image(image_url, f"{champion_id}.png")  # Pre-fetch images
-            return render_template('champions.html', champions=champions_data)
-
+            if not self.heores[self.language]:
+                for champion_id, champion in champions_data.items():
+                    image_url = f"https://ddragon.leagueoflegends.com/cdn/{self.latest_version}/img/champion/{champion_id}.png"
+                    self.fetch_image(image_url, f"{champion_id}.png")
+                self.heores[self.language] = champions_data
+            return render_template('champions.html', language=self.language, champions=champions_data, translations=self.translations[self.language])
 
         @self.app.route('/items')
         def items():
             items_data = self.get_data("item")
-            for items_id, item in items_data.items():
-                image_url = f"https://ddragon.leagueoflegends.com/cdn/{self.latest_version}/img/item/{items_id}.png"
-                self.fetch_image(image_url, f"{items_id}.png")  # Pre-fetch images
-            return render_template('items.html', items=items_data, fetch_image=self.fetch_image, latest_version=self.latest_version)
+            if not self.sorted_unique_items[self.language]:
+                unique_items = {}
+                for item_id, item in items_data.items():
+                    if item['maps'].get('11', False) and item["name"] not in unique_items:
+                        unique_items[item["name"]] = item
+                        unique_items[item["name"]]["id"] = item_id
+                        unique_items[item["name"]]["name"] = self.strip_html_tags(item['name'])
+                        image_url = f"https://ddragon.leagueoflegends.com/cdn/{self.latest_version}/img/item/{item_id}.png"
+                        self.fetch_image(image_url, f"{item_id}.png")
+                
+                self.sorted_unique_items[self.language] = sorted(unique_items.items(), key=lambda x: x[1]['gold']['total'])
+
+            return render_template('items.html', items=self.sorted_unique_items[self.language], language=self.language, fetch_image=self.fetch_image, translations=self.translations[self.language])
 
         @self.app.route('/champion/<champion_id>')
         def champion_details(champion_id):
@@ -48,7 +100,19 @@ class LeagueViewer:
             champion = champions_data.get(champion_id, None)
             if not champion:
                 return "Champion not found", 404
-            return render_template('champion_details.html', champion=champion, fetch_image=self.fetch_image)
+            for spell in champion['spells']:
+                spell['description'] = self.strip_html_tags(spell['description'])
+            return render_template('champion_details.html', champion=champion, language=self.language, translations=self.translations[self.language], fetch_image=self.fetch_image)
+
+        @self.app.route('/item/<item_id>')
+        def item_details(item_id):
+            item_data = self.get_data("item")
+            item = item_data.get(item_id, None)
+            if not item:
+                return "Champion not found", 404
+            item['description'] = self.strip_html_tags(item['description'])
+            return render_template('item_details.html', item_data=item_data, language=self.language, item=item, item_id=item_id, translations=self.translations[self.language], fetch_image=self.fetch_image)
+
 
         @self.app.route('/images/<path:image_name>')
         def serve_image(image_name):
@@ -76,6 +140,11 @@ class LeagueViewer:
             json.dump(response, file)
 
         return response
+
+    def strip_html_tags(self, text):
+        text = text.replace('<br>', '\n').replace('<br />', '\n').replace('<br/>', '\n')
+        clean = re.compile('<.*?>')
+        return re.sub(clean, '', text)
 
     def fetch_image(self, image_url, image_name):
         image_path = os.path.join(self.image_cache_dir, image_name)
